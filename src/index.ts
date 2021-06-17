99f2111ee4a9efbb975fd0d2725455f799c3ba8b
xebec19 <rohant946@gmail.com>
2021-06-18 00:27:57 +0530

    setting routes in application

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello');
+//dependencies
+const express = require('express');
+
+//routes
+const start = require ('./routes/public/start');
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use('/public/start',start);
+
+app.listen(port, () => {
+  console.log(`Hair-salon-backend server listening at http://localhost:${port}`)
+})
---
+++ src/routes/public/start.ts
@@ -0,0 +1,11 @@
+const router = express.Router();
+
+/**
+ * @type GET
+ * @path /public/start
+ * @description test response
+ * @access PUBLIC
+ */
+router.get('/',(req:any,res:any) => res.status(201).json({message:"ping ping ping"}));
+
+module.exports = router;
